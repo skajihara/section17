83a9f24cdbfe407b33eb55dd10c77c1df8f76c95
skajihara <t15cs015@gmail.com>
2024-04-03 16:30:43 +0900

    section17-1

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+function add(a: number, b: number) {
+  return a + b
+}
+</script>
+<template>
+  <h1>TypeScript</h1>
+  <button @click="add(3, 5)">add</button>
+</template>
