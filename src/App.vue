23055c13e0a2786c7449473f00c1b9b80d2b4001
skajihara <t15cs015@gmail.com>
2024-04-03 17:33:13 +0900

    section17-5

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-
-const count = ref(0)
-const doubleCount = computed(() => {
-  if (count.value > 3) return 100
-  return count.value * 2
-})
-doubleCount.value
-function countUp(event: MouseEvent) {
-  count.value++
-  console.log(event.clientX)
-}
+import ChildComp from './components/ChildComp.vue'
 </script>
 <template>
   <h1>TypeScript</h1>
-  <button @click="countUp">+1</button>
-  <p>count: {{ count }}</p>
+  <ChildComp :name="'A'" :level="10" />
 </template>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Course {
+  name?: string
+  level?: number
+}
+const props = withDefaults(defineProps<Course>(), {
+  name: 'default course',
+  level: 0
+})
+</script>
+<template>
+  <h2>ChildComp</h2>
+  <p>course name : {{ name }}</p>
+  <p>course level : {{ level }}</p>
+</template>
